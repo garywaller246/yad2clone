d5f0b4b8a79d8738c1491c02c47accd36739a7af
garywaller246 <garywaller246@gmail.com>
2019-07-17 12:24:43 +0300

    server working

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const serveStatic = require("serve-static");
+
+const path = require("path");
+
+// create the express app
+const app = express();
+
+//creat middleware to handle the serving of the app
+
+app.use("/", serveStatic(path.join(__dirname, "/public")));
+
+//create default port to serve the app
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log("server started on port " + port);
