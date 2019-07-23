ee8f2acf88a95e9003f36db22a90465303589e6a
garywaller246 <garywaller246@gmail.com>
2019-07-23 13:35:36 +0300

    Listings page HTML

--- public/js/components/yadtwo.js
+++ public/js/components/yadtwo.js
@@ -576,12 +576,163 @@ var Listings = function (_Component) {
         _react2.default.createElement(
           "div",
           { className: "container" },
-          "City: ",
-          match.params.city,
-          "Category: ",
-          match.params.category,
-          "Listings: ",
-          match.params.listings
+          _react2.default.createElement(
+            "section",
+            { className: "filter" },
+            _react2.default.createElement(
+              "div",
+              { className: "form-group price" },
+              _react2.default.createElement(
+                "label",
+                null,
+                "Price"
+              ),
+              _react2.default.createElement(
+                "select",
+                { name: "min-price", className: "min-price" },
+                _react2.default.createElement(
+                  "option",
+                  { value: "0" },
+                  "0"
+                )
+              ),
+              _react2.default.createElement(
+                "select",
+                { name: "max-price", className: "max-price" },
+                _react2.default.createElement(
+                  "option",
+                  { value: "1000" },
+                  "1000"
+                )
+              )
+            ),
+            _react2.default.createElement(
+              "div",
+              { className: "form-group make" },
+              _react2.default.createElement(
+                "label",
+                null,
+                "Make"
+              ),
+              _react2.default.createElement(
+                "select",
+                { name: "make", className: "make" },
+                _react2.default.createElement(
+                  "option",
+                  { value: "bmw" },
+                  "bmw"
+                )
+              )
+            ),
+            _react2.default.createElement(
+              "div",
+              { className: "form-group model" },
+              _react2.default.createElement(
+                "label",
+                null,
+                "Model"
+              ),
+              _react2.default.createElement(
+                "select",
+                { name: "model", className: "model" },
+                _react2.default.createElement(
+                  "option",
+                  { value: "bmw" },
+                  "bmw"
+                )
+              )
+            ),
+            _react2.default.createElement(
+              "div",
+              { className: "form-group button" },
+              _react2.default.createElement(
+                "div",
+                { className: "primary=btn" },
+                "Update"
+              ),
+              _react2.default.createElement(
+                "div",
+                { className: "reset=btn" },
+                "Reset"
+              )
+            )
+          ),
+          _react2.default.createElement(
+            "section",
+            { className: "list-view" },
+            _react2.default.createElement(
+              "section",
+              { className: "change-view" },
+              _react2.default.createElement(
+                "div",
+                { className: "form-group view-dropdown" },
+                _react2.default.createElement(
+                  "select",
+                  { name: "select-view", className: "select-view" },
+                  _react2.default.createElement(
+                    "option",
+                    { value: "gallery" },
+                    "Gallery View"
+                  ),
+                  _react2.default.createElement(
+                    "option",
+                    { value: "list" },
+                    "List View"
+                  ),
+                  _react2.default.createElement(
+                    "option",
+                    { value: "thumb" },
+                    "Thumb View"
+                  )
+                )
+              ),
+              _react2.default.createElement(
+                "div",
+                { className: "form-group sort-dropdown" },
+                _react2.default.createElement(
+                  "select",
+                  { name: "sort-dropdown", className: "sort-dropdown" },
+                  _react2.default.createElement(
+                    "option",
+                    { value: "newest" },
+                    "Newest"
+                  )
+                )
+              )
+            ),
+            _react2.default.createElement(
+              "section",
+              { className: "all-items" },
+              _react2.default.createElement(
+                "div",
+                { className: "item" },
+                _react2.default.createElement(
+                  "div",
+                  { className: "image" },
+                  _react2.default.createElement(
+                    "div",
+                    { className: "price" },
+                    "$8900"
+                  ),
+                  "image"
+                ),
+                _react2.default.createElement(
+                  "div",
+                  { className: "details" },
+                  _react2.default.createElement(
+                    "h5",
+                    null,
+                    "Title"
+                  ),
+                  _react2.default.createElement(
+                    "h6",
+                    null,
+                    "City"
+                  )
+                )
+              )
+            )
+          )
         )
       );
     }
